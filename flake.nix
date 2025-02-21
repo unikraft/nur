a27ffb99eaf18190ff08977dfd37044493a1074a
Julia Schlumpp <julia@unikraft.io>
2025-02-21 13:24:58 +0100

    feat: Also export packages using overlay
    Signed-off-by: Julia Schlumpp <julia@unikraft.io>

--- flake.nix
+++ flake.nix
@@ -10,5 +10,8 @@
         pkgs = import nixpkgs { inherit system; };
       });
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+      overlays = {
+        default = import ./overlay.nix;
+      };
     };
 }
